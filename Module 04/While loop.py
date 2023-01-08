adbf355741b206685920df1d9342d585815dc000
gmbanna <gmbanna@gmail.com>
2023-01-08 09:55:53 +0600

    final touch

---
+++ Module 04/While loop.py
@@ -0,0 +1,32 @@
+x = 1
+
+while x <= 10:
+    print(x)
+    x += 1
+
+x = 10
+
+while x > 0:
+    print('*' * x)
+    x -= 1
+banna = 0
+i = 1
+
+while i <= 5:
+    banna = banna + i
+    # print(i, '=' , sum)
+    i += 1
+print(banna)
+
+x = 10
+while x >= 0:
+    print('b' * x)
+    x -= 1
+
+banna = 0
+tuhin = 1
+while tuhin <= 5:
+    banna = banna + tuhin
+    print(tuhin, '=', banna)
+    tuhin += 1
+print(banna)
